9e1008bf5a217606e88e4e4dff28c71160163bce
Clément L <85962184+LILCLEMCLEM@users.noreply.github.com>
2023-12-26 13:41:20 +0100

    update

--- P12_Front-end-master/react-module/date-picker/src/lib/components/Calendar.jsx
+++ P12_Front-end-master/react-module/date-picker/src/lib/components/Calendar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "../styles.module.css";
-
+import { MonthInt2Str } from "../utils/setmonth";
 function Calendar() {
   let d = new Date();
   const [year, setYear] = useState();
@@ -8,20 +8,24 @@ function Calendar() {
 
   useEffect(() => {
     setYear(d.getYear() + 1900);
+    setMonth(d.getMonth());
   }, []);
 
-  const day = ["lu", "ma", "me", "je", "ve", "sa", "di"];
+  const day = ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"];
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.year}> 
+      <div className={styles.year}>
         <p onClick={(e) => setYear(year - 1)}>pre</p>
         <p>{year}</p>
         <p onClick={(e) => setYear(year + 1)}>sui</p>
       </div>
       <div className={styles.month}>
+        <p>{MonthInt2Str(month)}</p>
+      </div>
+      <div className={styles.day}>
         {day.map((item, index) => (
-          <p className={styles.Day} key={index}>
+          <p className={styles.day} key={index}>
             {item}
           </p>
         ))}
---
+++ P12_Front-end-master/react-module/date-picker/src/lib/utils/setmonth.jsx
@@ -0,0 +1,18 @@
+const monthStr = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jui",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
+
+export function MonthInt2Str(month) {
+  return monthStr[month - 1];
+}
